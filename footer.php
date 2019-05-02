9b772bd8f3a57c6ffa1a31643a77ea75f585c529
Unknown <ajlanisenda53@gmail.com>
2019-05-02 13:52:41 +0100

    debut

---
+++ footer.php
@@ -0,0 +1,207 @@
+<footer class="sit-footer">
+    <div class="outer-container">
+        <div class="container-fluid">
+            <div class="row footer-recent-posts">
+                <div class="col-12 col-md-6 col-xl-3">
+                    <div class="footer-post-wrap flex justify-content-between">
+                        <figure>
+                            <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/foot-1.jpg" alt=""></a>
+                        </figure>
+
+                        <div class="footer-post-cont flex flex-column justify-content-between">
+                            <header class="entry-header">
+                                <div class="posted-date">
+                                    January 30, 2018
+                                </div><!-- .entry-header -->
+
+                                <h3><a href="#">My fall in love story</a></h3>
+
+                                <div class="tags-links">
+                                    <a href="#">#winter</a>
+                                    <a href="#">#love</a>
+                                    <a href="#">#snow</a>
+                                    <a href="#">#january</a>
+                                </div><!-- .tags-links -->
+                            </header><!-- .entry-header -->
+
+                            <footer class="entry-footer">
+                                <a class="read-more" href="#">read more</a>
+                            </footer><!-- .entry-footer -->
+                        </div><!-- .footer-post-cont -->
+                    </div><!-- .footer-post-wrap -->
+                </div><!-- .col -->
+                <div class="col-12 col-md-6 col-xl-3">
+                    <div class="footer-post-wrap flex justify-content-between">
+                        <figure>
+                            <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/foot-2.jpg" alt=""></a>
+                        </figure>
+
+                        <div class="footer-post-cont flex flex-column justify-content-between">
+                            <header class="entry-header">
+                                <div class="posted-date">
+                                    January 30, 2018
+                                </div><!-- .entry-header -->
+
+                                <h3><a href="#">Man’s best friend</a></h3>
+
+                                <div class="tags-links">
+                                    <a href="#">#winter</a>
+                                    <a href="#">#love</a>
+                                    <a href="#">#snow</a>
+                                    <a href="#">#january</a>
+                                </div><!-- .tags-links -->
+                            </header><!-- .entry-header -->
+
+                            <footer class="entry-footer">
+                                <a class="read-more" href="#">read more</a>
+                            </footer><!-- .entry-footer -->
+                        </div><!-- .footer-post-cont -->
+                    </div><!-- .footer-post-wrap -->
+                </div><!-- .col -->
+
+                <div class="col-12 col-md-6 col-xl-3">
+                    <div class="footer-post-wrap flex justify-content-between">
+                        <figure>
+                            <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/foot-3.jpg" alt=""></a>
+                        </figure>
+
+                        <div class="footer-post-cont flex flex-column justify-content-between">
+                            <header class="entry-header">
+                                <div class="posted-date">
+                                    January 30, 2018
+                                </div><!-- .entry-header -->
+
+                                <h3><a href="#">Writing on a budget</a></h3>
+
+                                <div class="tags-links">
+                                    <a href="#">#winter</a>
+                                    <a href="#">#love</a>
+                                    <a href="#">#snow</a>
+                                    <a href="#">#january</a>
+                                </div><!-- .tags-links -->
+                            </header><!-- .entry-header -->
+
+                            <footer class="entry-footer">
+                                <a class="read-more" href="#">read more</a>
+                            </footer><!-- .entry-footer -->
+                        </div><!-- .footer-post-cont -->
+                    </div><!-- .footer-post-wrap -->
+                </div><!-- .col -->
+
+                <div class="col-12 col-md-6 col-xl-3">
+                    <div class="footer-post-wrap flex justify-content-between">
+                        <figure>
+                            <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/foot-4.jpg" alt=""></a>
+                        </figure>
+
+                        <div class="footer-post-cont flex flex-column justify-content-between">
+                            <header class="entry-header">
+                                <div class="posted-date">
+                                    January 30, 2018
+                                </div><!-- .entry-header -->
+
+                                <h3><a href="#">My fall in love story</a></h3>
+
+                                <div class="tags-links">
+                                    <a href="#">#winter</a>
+                                    <a href="#">#love</a>
+                                    <a href="#">#snow</a>
+                                    <a href="#">#january</a>
+                                </div><!-- .tags-links -->
+                            </header><!-- .entry-header -->
+
+        <footer class="entry-footer">
+      <a class="read-more" href="#">read more</a>
+                            </footer><!-- .entry-footer -->
+                        </div><!-- .footer-post-cont -->
+                    </div><!-- .footer-post-wrap -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container-fluid -->
+    </div><!-- .outer-container -->
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="footer-instagram flex flex-wrap flex-lg-nowrap">
+                <figure>
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/a.jpg" alt=""></a>
+                </figure>
+
+                <figure>
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/b.jpg" alt=""></a>
+                </figure>
+
+                <figure>
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/c.jpg" alt=""></a>
+                </figure>
+
+                <figure>
+                        <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/d.jpg" alt=""></a>
+                </figure>
+
+                <figure>
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/e.jpg" alt=""></a>
+                </figure>
+
+                <figure>
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/f.jpg" alt=""></a>
+                </figure>
+
+                <figure>
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/g.jpg" alt=""></a>
+                </figure>
+            </div>
+        </div><!-- .row -->
+    </div><!-- .container -->
+
+    <div class="footer-bar">
+        <div class="outer-container">
+            <div class="container-fluid">
+                <div class="row justify-content-between">
+                    <div class="col-12 col-md-6">
+                        <div class="footer-copyright">
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </div><!-- .footer-copyright -->
+                    </div><!-- .col-xl-4 -->
+<footer class="entry-footer flex flex-column flex-lg-row justify-content-between align-content-start align-lg-items-center">
+                        <ul class="post-share flex align-items-center order-3 order-lg-1">
+                            <label>Share</label>
+                            <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        </ul><!-- .post-share -->
+
+                        <a class="read-more order-2" href="#">Read more</a>
+
+                        <div class="comments-count order-1 order-lg-3">
+                            <a href="#">2 Comments</a>
+                        </div><!-- .comments-count -->
+
+                    </footer><!-- .entry-footer -->
+                    <div class="col-12 col-md-6">
+                        <div class="footer-social">
+                            <ul class="flex justify-content-center justify-content-md-end align-items-center">
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                        </div><!-- .footer-social -->
+                    </div><!-- .col-xl-4 -->
+                </div><!-- .row -->
+            </div><!-- .container-fluid -->
+        </div><!-- .outer-container -->
+    </div><!-- .footer-bar -->
+</footer><!-- .sit-footer -->
+<script type='text/javascript' src="<?php bloginfo('template_directory');?>/js/jquery.js"
+rel="stylesheet">'></script>
+<script type='text/javascript' src="<?php bloginfo('template_directory'); ?>/js/swiper.min.js"
+rel="stylesheet">'></script>
+<script type='text/javascript' src="<?php bloginfo('template_directory'); ?>/js/custom.js"
+rel="stylesheet">'></script>
